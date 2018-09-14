bc1f221f5cb96f83d9996a11fcafc60efcb60648
Samujjal Choudhury <35115461+SamujjalChoudhury@users.noreply.github.com>
2018-09-15 01:20:54 +0530

    Add files via upload

---
+++ JOHNY.py
@@ -0,0 +1,37 @@
+#Problem Question
+'''Vlad enjoys listening to music. He lives in Sam's Town. A few
+days ago he had a birthday, so his parents gave him a gift: MP3-player!
+Vlad was the happiest man in the world! Now he can listen his favorite
+songs whenever he wants!
+
+Vlad built up his own playlist. The playlist consists of N songs, each
+has a unique positive integer length. Vlad likes all the songs from his
+playlist, but there is a song, which he likes more than the others.
+It's named "Uncle Johny".
+
+After creation of the playlist, Vlad decided to sort the songs in increasing
+order of their lengths. For example, if the lengths of the songs in playlist
+was {1, 3, 5, 2, 4} after sorting it becomes {1, 2, 3, 4, 5}. Before the
+sorting, "Uncle Johny" was on K-th position (1-indexing is assumed for the
+playlist) in the playlist.
+
+Vlad needs your help! He gives you all the information of his playlist.
+Your task is to find the position of "Uncle Johny" in the sorted playlist.'''
+
+
+
+cases = int(input())
+
+for case in range(cases):
+    songs = int(input())
+    playlist = list(map(int, input().split()))
+    k = int(input())
+    uncle_johny = playlist[k-1]
+    playlist.sort()
+    for song in range(len(playlist)):
+        if playlist[song] == uncle_johny:
+            uncle = song+1
+            break
+    print(uncle)
+    del playlist[:]
+          
